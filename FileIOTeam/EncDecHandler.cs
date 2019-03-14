6e38d8bb049c09a62f1a021f8fb88779820028d7
ATimko <ashleytimko777@gmail.com>
2019-03-14 09:36:28 -0700

    Add project files.

---
+++ FileIOTeam/EncDecHandler.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileIOTeam
+{
+    class EncDecHandler
+    {
+        public static string EncryptString(string ClearText)
+        {
+            byte[] clearTextBytes = Encoding.UTF8.GetBytes(ClearText);
+            System.Security.Cryptography.SymmetricAlgorithm rijn = SymmetricAlgorithm.Create();
+            MemoryStream ms = new MemoryStream();
+
+            string key1 = "aaaaaaaaaaaaaaaa";
+            string key2 = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+
+            byte[] rgbIV = Encoding.ASCII.GetBytes(key1);
+            byte[] key = Encoding.ASCII.GetBytes(key2);
+            CryptoStream cs = new CryptoStream(ms, rijn.CreateEncryptor(key, rgbIV), CryptoStreamMode.Write);
+            cs.Write(clearTextBytes, 0, clearTextBytes.Length);
+            cs.Close();
+
+            return Convert.ToBase64String(ms.ToArray());
+        }
+
+        public static string DecryptString(string EncryptText)
+        {
+            byte[] encryptedTextBytes = Convert.FromBase64String(EncryptText);
+            MemoryStream ms = new MemoryStream();
+            System.Security.Cryptography.SymmetricAlgorithm rijn = SymmetricAlgorithm.Create();
+
+            string key1 = "aaaaaaaaaaaaaaaa";
+            string key2 = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+
+            byte[] rgbIV = Encoding.ASCII.GetBytes(key1);
+            byte[] key = Encoding.ASCII.GetBytes(key2);
+            CryptoStream cs = new CryptoStream(ms, rijn.CreateDecryptor(key, rgbIV), CryptoStreamMode.Write);
+            cs.Write(encryptedTextBytes, 0, encryptedTextBytes.Length);
+            cs.Close();
+            return Encoding.UTF8.GetString(ms.ToArray());
+        }
+    }
+}
---
+++ FileIOTeam/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FileIOTeam
+{
+    public partial class Form1 : Form
+    {
+        private OpenFileDialog openFileDialog1;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnchoosefile_Click(object sender, EventArgs e)
+        {
+            openFileDialog1 = new OpenFileDialog();
+            // only Text file
+            openFileDialog1.Filter = "txt files (*.txt)|";
+
+            try
+            {
+                if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    textBox1.Text = openFileDialog1.FileName;
+
+                }
+                else
+                {
+                    MessageBox.Show("file is empty");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!string.IsNullOrEmpty(textBox1.Text))
+                {
+                    string text = File.ReadAllText(textBox1.Text);
+
+                    var enctext = EncDecHandler.EncryptString(text);
+
+                    TextWriter txt = new StreamWriter(@"C:\\demoEnc.txt");
+                    txt.Write(enctext);
+                    txt.Close();
+
+                    MessageBox.Show("file is Encrypted successfully Location : C:\\demoEnc.txt");
+                    textBox1.Clear();
+                }
+                else
+                {
+                    MessageBox.Show("file is empty");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!string.IsNullOrEmpty(textBox1.Text))
+                {
+                    string text = File.ReadAllText(textBox1.Text);
+
+                    var enctext = EncDecHandler.DecryptString(text);
+
+                    TextWriter txt = new StreamWriter(@"C:\\demoDec.txt");
+                    txt.Write(enctext);
+                    txt.Close();
+
+                    MessageBox.Show("file is Decrypted successfully Location : C:\\demoDec.txt");
+                    textBox1.Clear();
+                }
+                else
+                {
+                    MessageBox.Show("file is empty");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
